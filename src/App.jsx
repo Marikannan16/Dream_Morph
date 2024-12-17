74a861413ac17d480321f2593705c22703d27b73
marikannan16 <capkannan16@gmail.com>
2024-12-17 17:14:23 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,4 @@
 import React from 'react'
-import GsapTo from './Gsaptutorial/GsapTo'
-import GsapFrom from './Gsaptutorial/GsapFrom'
-import GsapfromTo from './Gsaptutorial/GsapfromTo'
-import GsapTimeline from './Gsaptutorial/GsapTimeline'
-import CardContainer from './Gsaptutorial/GsapStagger'
-import ThreeDTut from './3Dtut/ThreeDTut'
-import Chatbot from './huggingface/Chat'
-import GsapStagger from './Gsaptutorial/GsapStagger'
-import GsapScroll from './Gsaptutorial/GsapScroll'
-import Portfolio from './Portfolio/Portfolio'
 import ImageGenerator from './microsoftdesignerclone/ImageGenerator'
 
 const App = () => {
